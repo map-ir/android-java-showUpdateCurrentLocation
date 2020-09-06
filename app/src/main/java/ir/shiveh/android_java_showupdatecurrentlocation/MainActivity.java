f6f545be50a8125b031097bf9e5637e9f80e6361
Morteza Hosseini <mortezahosseinee@gmail.com>
2020-09-06 11:58:47 +0430

    initial commit

---
+++ app/src/main/java/ir/shiveh/android_java_showupdatecurrentlocation/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.shiveh.android_java_showupdatecurrentlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
